5bfcb02720d4b6aa121180eb219709c841d513f7
Jibril M Jibril <jibrilmjibril05@gmail.com>
2025-04-03 17:40:37 +0100

    2

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     full_name = Column(String)
     program = Column(String)
